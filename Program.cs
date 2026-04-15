537c3f4de9a70363e0f6ca0162df88fd6bcb90d9
unknown <jonas-silva8@outlook.com>
2026-04-15 14:09:20 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+namespace jwtkey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var keyBytes = RandomNumberGenerator.GetBytes(64);
+            var key = Convert.ToBase64String(keyBytes);
+
+            Console.WriteLine($"jwt_key: {key}");
+        }
+    }
+}
